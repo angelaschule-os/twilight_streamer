e28a1f46fc386ba70c075b807ac49785a5776f8f
BjoernSchilberg <bjoern.schilberg@posteo.de>
2023-04-16 07:59:04 +0200

    Added Raspberry check. Rename recoder to streamer.

--- build.sh
+++ build.sh
@@ -9,15 +9,15 @@ git_hash=$(git rev-parse --short HEAD)
 echo "GIT_HASH = '${git_hash}'" > git_version.py
 
 # Build the dynamic executable binary with PyInstaller
-python -m PyInstaller --onefile --name twilight_recorder main.py
+python -m PyInstaller --onefile --name twilight_streamer main.py
 
 # Bundle dynamic executables with their library dependencies so they can be run
 # anywhere, just like a static executable.
 
-if [ -z "$GITHUB_ACTION" ]
-then
-  echo "GITHUB_ACTION is not present. staticx can be run."
-  python -m staticx dist/twilight_recorder dist/twilight_recorder
+if grep -q 'Raspberry' /proc/cpuinfo || [ -n "$GITHUB_ACTION" ]; then
+    echo "This system is a Raspberry Pi or the script is running in a GitHub Actions workflow."
+    echo "staticx cannot be run."
 else
-  echo "GITHUB_ACTION is present. staticx cannot be run in a GitHub Action environment."
+  echo "GITHUB_ACTION is not present. staticx can be run."
+  python -m staticx dist/twilight_streamer dist/twilight_streamer
 fi
